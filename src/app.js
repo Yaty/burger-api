b15e9a5161005088af80886d8ff951365d5cbca3
Da Roit Hugo <contact@hdaroit.fr>
2018-05-13 12:07:35 +0200

    Add rate limiter

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const RateLimiter = require('express-rate-limit');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const projectInfo = require('../package.json');
 const logger = require('./utils/logger')('app');
 
 const app = express();
+
+app.use(new RateLimiter({
+    windowMs: 15*60*1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+    delayMs: 0, // disable delaying - full speed until the max limit is reached
+}));
 
 app.use(morgan('combined', {
   stream: {
