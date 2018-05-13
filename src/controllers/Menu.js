5dc79f0099978881af2051b74569b16e51654e4a
Da Roit Hugo <contact@hdaroit.fr>
2018-05-13 22:03:41 +0200

    Attach promotions

--- src/controllers/Menu.js
+++ src/controllers/Menu.js
@@ -5,8 +5,9 @@ const crud = require('./utils/crud')(Menu);
 module.exports = {
     ...crud,
     async updateById(id, data) {
-        const productIds = data.productIds;
+        const {productIds, promotionIds} = data;
         delete data.productIds;
+        delete data.promotionIds;
 
         const instance = await crud.updateById(id, data, false);
         if (_.isNil(instance)) return;
@@ -15,17 +16,26 @@ module.exports = {
             await instance.products().attach(productIds);
         }
 
+        if (_.isArray(promotionIds)) {
+            await instance.promotions().attach(promotionIds);
+        }
+
         return instance.toJSON();
     },
     async create(data) {
-        const productIds = data.productIds;
+        const {productIds, promotionIds} = data;
         delete data.productIds;
+        delete data.promotionIds;
 
         const instance = await crud.create(data, false);
         if (_.isNil(instance)) return;
 
         if (_.isArray(productIds)) {
             await instance.products().attach(productIds);
+        }
+
+        if (_.isArray(promotionIds)) {
+            await instance.promotions().attach(promotionIds);
         }
 
         return instance.toJSON();
--- src/controllers/Product.js
+++ src/controllers/Product.js
@@ -1,4 +1,33 @@
+const _ = require('lodash');
 const {Product} = require('../models');
-const crud = require('./utils/crud');
+const crud = require('./utils/crud')(Product);
 
-module.exports = crud(Product);
+module.exports = {
+    ...crud,
+    async updateById(id, data) {
+        const {promotionIds} = data;
+        delete data.promotionIds;
+
+        const instance = await crud.updateById(id, data, false);
+        if (_.isNil(instance)) return;
+
+        if (_.isArray(promotionIds)) {
+            await instance.promotions().attach(promotionIds);
+        }
+
+        return instance.toJSON();
+    },
+    async create(data) {
+        const {promotionIds} = data;
+        delete data.promotionIds;
+
+        const instance = await crud.create(data, false);
+        if (_.isNil(instance)) return;
+
+        if (_.isArray(promotionIds)) {
+            await instance.promotions().attach(promotionIds);
+        }
+
+        return instance.toJSON();
+    },
+};
