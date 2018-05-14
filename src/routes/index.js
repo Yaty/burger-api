94fb8c5f62fe0c02238779504b5aa028d5ae0fa4
Da Roit Hugo <contact@hdaroit.fr>
2018-05-14 23:46:48 +0200

    Last update !

--- src/routes/index.js
+++ src/routes/index.js
@@ -43,29 +43,26 @@ router.use('/products', products);
 router.use('/users', users);
 router.use('/promotions', promotions);
 
-if (process.env.NODE_ENV === 'development') {
-    const packageJson = require('../../package.json');
-    const path = require('path');
-    const swaggerUi = require('swagger-ui-express');
-    const basePath = path.resolve(__dirname, '..');
-    const swaggerJSDoc = require('swagger-jsdoc')({
-        swaggerDefinition: {
-            info: {
-                title: packageJson.name,
-                version: packageJson.version,
-            },
-            host: 'localhost:3000',
-            basePath: '/v' + packageJson.version.split('.')[0],
+const packageJson = require('../../package.json');
+const path = require('path');
+const swaggerUi = require('swagger-ui-express');
+const basePath = path.resolve(__dirname, '..');
+const swaggerJSDoc = require('swagger-jsdoc')({
+    swaggerDefinition: {
+        info: {
+            title: packageJson.name,
+            version: packageJson.version,
         },
-        apis: [
-            path.join(basePath, 'routes/**/*.js'),
-            path.join(basePath, 'routes/*.js'),
-        ],
-    });
+        host: 'localhost:3000',
+        basePath: '/v' + packageJson.version.split('.')[0],
+    },
+    apis: [
+        path.join(basePath, 'routes/**/*.js'),
+        path.join(basePath, 'routes/*.js'),
+    ],
+});
 
-    router.use('/explorer', swaggerUi.serve, swaggerUi.setup(swaggerJSDoc));
-}
-
+router.use('/explorer', swaggerUi.serve, swaggerUi.setup(swaggerJSDoc));
 router.use(error(logger));
 
 module.exports = router;
